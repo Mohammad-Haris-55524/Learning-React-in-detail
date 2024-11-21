6fe561fdb66991296c68448acd689e0f697d0b64
Mohammad-Haris-55524 <mohammadharis10678@gmail.com>
2024-11-21 23:45:36 +0500

    Life cycle methods basic + constructor method

---
+++ LifeCycle methods in React/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import BasicsOfLifeCycleMethods from './components/BasicsOfLifeCycleMethods'
+import Constructor from './components/Constructor'
+
+function App() {
+
+return (
+<>
+<BasicsOfLifeCycleMethods/>
+{/* <Constructor/> */}
+</>
+  )
+}
+
+export default App
