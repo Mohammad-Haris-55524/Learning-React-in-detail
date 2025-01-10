9d0e56446b24e15ab4542e58efb9ee4d9681b990
Mohammad-Haris-55524 <mohammadharis10678@gmail.com>
2025-01-10 20:15:28 +0500

    Learning basic table in react

---
+++ React Self practise/Learning React Form and how to handle pagination in react form data/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import TableCurdUsingAntd from './components/tableCurdUsingAntd'
+// import DynamicModalComponenet from './components/DynamicModalComponent'
+
+
+function App() {
+
+  return (
+    <>
+   {/* <BasicTable/> */}
+   <TableCurdUsingAntd/>
+    </>
+  )
+}
+
+export default App
