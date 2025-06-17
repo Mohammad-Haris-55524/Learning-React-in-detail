3e9c35e9024c99e78ce54ba9d658e2ec180230ae
Mohammad Haris <mohammadharis10678@gmail.com>
2025-06-17 11:55:28 -0700

    How to use RTK Query with a fake e-commerce API

---
+++ Introduction to RTK Query/vite-project/src/components/ProductList.jsx
@@ -0,0 +1,27 @@
+// src/components/ProductsList.js
+import React from 'react'
+import { useGetProductsQuery } from '../services/fakeStoreApi'
+
+function ProductsList() {
+  const { data: products, error, isLoading } = useGetProductsQuery()
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>Error: {error.message}</div>
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <h3>{product.title}</h3>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.title} width="100" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default ProductsList
---
+++ Introduction to RTK Query/vite-project/src/store/index.js
@@ -0,0 +1,11 @@
+// src/app/store.js
+import { configureStore } from '@reduxjs/toolkit'
+import { fakeStoreApi } from '../services/fakeStoreApi'
+
+export const store = configureStore({
+  reducer: {
+    [fakeStoreApi.reducerPath]: fakeStoreApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(fakeStoreApi.middleware),
+})
