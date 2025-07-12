d63ecc16832960213367ba1a11574e031e0c9678
Mohammad Haris <mohammadharis10678@gmail.com>
2025-07-12 11:51:28 -0700

    Delete product mutation has been created

--- Introduction to RTK Query/vite-project/src/store/features/apiSlice.js
+++ Introduction to RTK Query/vite-project/src/store/features/apiSlice.js
@@ -67,33 +67,89 @@
 
 // src/services/fakeStoreApi.js
 // src/services/fakeStoreApi.js
+
+// -----------------------------------------------------------------------------------------------------------------------------------
+// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// export const fakeStoreApi = createApi({
+//   reducerPath: 'fakeStoreApi',
+//   baseQuery: fetchBaseQuery({ baseUrl: 'https://fakestoreapi.com/' }),
+//   tagTypes: ['Product'], // For cache invalidation
+//   endpoints: (builder) => ({
+//     // GET all products
+//     getProducts: builder.query({
+//       query: () => 'products',
+//       providesTags: ['Product'] // This data can be tagged
+//     }),
+    
+//     // POST new product
+//     addProduct: builder.mutation({
+//       query: (newProduct) => ({
+//         url: 'products',
+//         method: 'POST',
+//         body: newProduct
+//       }),
+//       invalidatesTags: ['Product'] // This will refetch products
+//     }),
+//   }),
+// })
+
+// // Export hooks
+// export const { 
+//   useGetProductsQuery,
+//   useAddProductMutation
+// } = fakeStoreApi
+
+// -----------------------------------------------------------------------------------------------------------------------------------
+
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const fakeStoreApi = createApi({
   reducerPath: 'fakeStoreApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://fakestoreapi.com/' }),
-  tagTypes: ['Product'], // For cache invalidation
+  tagTypes: ['Product'],
   endpoints: (builder) => ({
-    // GET all products
     getProducts: builder.query({
       query: () => 'products',
-      providesTags: ['Product'] // This data can be tagged
+      providesTags: ['Product']
     }),
     
-    // POST new product
     addProduct: builder.mutation({
       query: (newProduct) => ({
         url: 'products',
         method: 'POST',
         body: newProduct
       }),
-      invalidatesTags: ['Product'] // This will refetch products
+      invalidatesTags: ['Product']
     }),
+    
+    // NEW: Delete product endpoint
+    deleteProduct: builder.mutation({
+      query: (id) => ({
+        url: `products/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Product'],
+      // Optimistic update (optional)
+      async onQueryStarted(id, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          fakeStoreApi.util.updateQueryData('getProducts', undefined, draft => {
+            return draft.filter(product => product.id !== id)
+          })
+        )
+        try {
+          await queryFulfilled
+        } catch {
+          patchResult.undo()
+        }
+      }
+    })
   }),
 })
 
-// Export hooks
+// Export all hooks
 export const { 
   useGetProductsQuery,
-  useAddProductMutation
+  useAddProductMutation,
+  useDeleteProductMutation // NEW
 } = fakeStoreApi
